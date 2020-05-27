e574906b516154ce246ff00dea5d89a95095dc5f
Prasanthvel <prasanthvel@gmail.com>
2020-05-28 01:03:16 +0530

    Initial Commits
    Basic Event Handler implemented

---
+++ Assets/scripts/HandlerA.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandlerA : MonoBehaviour
+{
+
+    private void OnEnable()
+    {
+        EventManager.StartListening(EventManager.TRIGGER_A, EventTriggerFromB);
+    }
+
+    private void OnDisable()
+    {
+        EventManager.StopListening(EventManager.TRIGGER_A, EventTriggerFromB);
+    }
+
+    void EventTriggerFromB() {
+        Debug.Log("this method in Handler A is triggered from HandlerB");
+    }
+
+    public void BtnHandlerA()
+    {
+        EventManager.TriggerEvent(EventManager.TRIGGER_B);
+    }
+}
---
+++ Assets/scripts/HandlerB.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandlerB : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        EventManager.StartListening(EventManager.TRIGGER_B, EventTriggerFromA);
+    }
+
+    private void OnDisable()
+    {
+        EventManager.StopListening(EventManager.TRIGGER_B, EventTriggerFromA);
+    }
+
+    void EventTriggerFromA()
+    {
+        Debug.Log("this method in Handler B is triggered from HandlerA");
+    }
+
+    public void BtnHandlerB() {
+        EventManager.TriggerEvent(EventManager.TRIGGER_A);
+    }
+}
